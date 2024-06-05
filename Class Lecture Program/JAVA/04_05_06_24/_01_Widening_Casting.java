cf4b1c8beea18e6a9cb27a72b0216296e69d4260
Sushantlog <sushantbpatil53@gmail.com>
2024-06-05 18:02:17 +0530

    first commit

---
+++ Class Lecture Program/JAVA/04_05_06_24/_01_Widening_Casting.java
@@ -0,0 +1,9 @@
+public class _01_Widening_Casting {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
---
+++ Class Lecture Program/JAVA/04_05_06_24/_02_Narrowing.java
@@ -0,0 +1,9 @@
+public class _02_Narrowing {
+    public static void main(String[] args) {
+        double myDouble = 5.63d;
+        int myInt = (int)myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
